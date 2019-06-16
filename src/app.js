e9e8fadba7749e575cdf38ca6ee8a571bdcb7c29
Ian <ian.m.trent@gmail.com>
2019-06-15 22:30:59 -0400

    add: Hamburger toggle animation

--- src/app.js
+++ src/app.js
@@ -4,3 +4,13 @@ import Bubblies from './components/Bubblies';
 import './styles/styles.scss';
 
 ReactDOM.render(<Bubblies />, document.getElementById('bubblies'));
+
+const hamburgerToggle = () => {
+	const burger = document.querySelector('.hamburger');
+
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('toggle');
+	});
+};
+
+hamburgerToggle();
