6653909ed2f6712cbfa789764e1b8184513033da
Ian Trent <ian.m.trent@gmail.com>
2019-10-26 20:15:54 -0400

    Moved all themes into one object

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,26 +1,26 @@
-export const Ayu = {
-    background: '#0A0E14',
-    text: '#B3B1AD',
-    elem: '#39BAE6',
-    attribute: '#C1D84B',
-    identifier: '#FB8C3F',
-    variable: '#FFB454'
-};
-
-export const Cobalt = {
-    background: '#193549',
-    text: '#FFFFFF',
-    elem: '#9EFFFF',
-    attribute: '#97F47D',
-    identifier: '#D2AA0E',
-    variable: '#FF9D00'
-};
-
-export const Material = {
-    background: '#263238',
-    text: '#EEFFFF',
-    elem: '#F07178',
-    attribute: '#A4C57C',
-    identifier: '#C792EA',
-    variable: '#82AAFF'
+export default {
+    ayu: {
+        background: '#0A0E14',
+        text: '#B3B1AD',
+        elem: '#39BAE6',
+        attribute: '#C1D84B',
+        identifier: '#FB8C3F',
+        variable: '#FFB454'
+    },
+    cobalt: {
+        background: '#193549',
+        text: '#FFFFFF',
+        elem: '#9EFFFF',
+        attribute: '#97F47D',
+        identifier: '#D2AA0E',
+        variable: '#FF9D00'
+    },
+    material: {
+        background: '#263238',
+        text: '#EEFFFF',
+        elem: '#F07178',
+        attribute: '#A4C57C',
+        identifier: '#C792EA',
+        variable: '#82AAFF'
+    }
 };
