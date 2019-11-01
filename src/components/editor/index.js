a0eed7e98ea116f2dc144a8d1026c3cb0ae7ba82
Ian Trent <ian.m.trent@gmail.com>
2019-11-01 19:47:34 -0400

    Changed prop data on editor

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -3,12 +3,12 @@ import { ThemeProvider } from 'styled-components';
 import TabBar from '../tabBar';
 import { Wrapper } from './style';
 
-const Editor = ({ settings }) => {
+const Editor = ({ theme, browserSettings }) => {
     return (
-        <ThemeProvider theme={settings.theme}>
-            <Wrapper border={settings.browser.border}>
-                {settings.browser.tabBar && (
-                    <TabBar border={settings.browser.border} />
+        <ThemeProvider theme={theme}>
+            <Wrapper border={browserSettings.border}>
+                {browserSettings.tabBar && (
+                    <TabBar border={browserSettings.border} />
                 )}
             </Wrapper>
         </ThemeProvider>
