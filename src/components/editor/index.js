77b4cb6d233f60d8504315ba0447539b9777b63e
Ian Trent <ian.m.trent@gmail.com>
2019-11-04 22:50:14 -0500

    Rendered bubbles within editor

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import TabBar from '../tabBar';
 import { Wrapper } from './style';
+import Bubble from '../bubble';
 
 const Editor = ({ theme, browserSettings }) => {
     return (
         <ThemeProvider theme={theme}>
-            <Wrapper border={browserSettings.border}>
+            <Wrapper border={browserSettings.border} className="editor">
                 {browserSettings.tabBar && (
                     <TabBar border={browserSettings.border} />
                 )}
+                {Array(10).fill(<Bubble />)}
             </Wrapper>
         </ThemeProvider>
     );
