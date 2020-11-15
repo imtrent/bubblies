1545946170496d475a14acc30e40c3fb5f7edc72
Ian Trent <ian.m.trent@gmail.com>
2020-11-14 19:45:52 -0500

    fix: make context more discoverable by ts

--- src/store/SettingsContext.tsx
+++ src/store/SettingsContext.tsx
@@ -1,15 +1,29 @@
-import * as React from 'react';
+import React from 'react';
 
-interface ISettings {
+type Settings = {
   controls: boolean;
   borderRadius: number;
   theme: string;
-}
+};
 
-const SettingsContext = React.createContext({});
+type SettingsContextProps = {
+  settings: Settings;
+  setSettings: React.Dispatch<any>;
+};
+
+const initialState: Settings = {
+  controls: true,
+  borderRadius: 3,
+  theme: 'Ayu'
+};
+
+const SettingsContext = React.createContext<SettingsContextProps>({
+  settings: initialState,
+  setSettings: () => initialState
+});
 
 export const SettingsProvider: React.FC = ({ children }) => {
-  const [settings, setSettings] = React.useState<ISettings>({
+  const [settings, setSettings] = React.useState<Settings>({
     controls: true,
     borderRadius: 3,
     theme: 'Ayu'
@@ -22,7 +36,10 @@ export const SettingsProvider: React.FC = ({ children }) => {
   );
 };
 
-export function useSettings() {
+export default function useSettings() {
   const context = React.useContext(SettingsContext);
+  if (!context) {
+    throw new Error(`useSettings must be used within a SettingsProvider`);
+  }
   return context;
 }
